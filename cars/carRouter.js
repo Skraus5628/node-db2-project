7c6708568373f1b8c2e26fdb9c947b810b36a0a9
skraus5628 <s_read@aol.com>
2020-02-29 12:11:27 -0600

    set up db file and router paths

---
+++ cars/carRouter.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const db = require("../data/config")
+
+const router = express.Router()
+
+router.get("/", async (req, res, next) =>{
+
+})
+
+router.get("/:id", async (req, res, next) =>{
+
+})
+
+router.post("/", async (req, res, next) =>{
+
+})
+
+module.exports = router
