c6c1e863dc759de5d60e74a7d23d4a57059ab753
skraus5628 <s_read@aol.com>
2020-02-29 13:18:46 -0600

    MVP, with funcitonal seed file

--- seeds/cars.js
+++ seeds/cars.js
@@ -1,13 +1,15 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async function(knex) {
+
+  await knex("cars").truncate()
+
+  await knex("cars").insert([
+    { VIN: 123456, make: "Kia", model: "Soul", mileage:156000, transmission: "manual", titleStatus: "salvage" },
+    { VIN: 789123, make: "Dodge", model: "Charger", mileage:120365, transmission: "automatic" },
+    { VIN: 456789, make: "Cadillac", model: "DeVille", mileage:200505, transmission: "manual", titleStatus: "clean" },
+    { VIN: 156485, make: "Toyota", model: "Camry", mileage:140000 },
+    { VIN: 802465, make: "Ford", model: "Mustang", mileage:56000, transmission: "automatic"},
+    { VIN: 045155, make: "Jeep", model: "Wrangler", mileage:75658 },
+  ]);
+
 };
